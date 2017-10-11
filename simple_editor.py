49a394a73777ff408f8a1fe04e7d764b2da848c1
Truong Van Thanh <van.truong@insa-cvl.fr>
2017-10-12 00:41:56 +0200

    Add files via upload

--- simple_editor.py
+++ simple_editor.py
@@ -9,61 +9,62 @@ How to use:
 Make sure you run this program with python 3(recommended version 3.4 or newer)
 """
 from tkinter import *
-
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import os
 def quit():
     """
     quit callback function: simply exit the program.
     """
     exit()
-def cut_callback(context_text):
+def cut_callback(content_text):
     """
     cut_callback: implement cut function(I just used built-in cut function of Text object)
-    context_text: Text object
+    content_text: Text object
     """
-    context_text.event_generate('<<Cut>>')
-def copy_callback(context_text):
+    content_text.event_generate('<<Cut>>')
+def copy_callback(content_text):
     """
     copy callback: implement copy function
-    context_text: Text object
+    content_text: Text object
     """
-    context_text.event_generate('<<Copy>>')
-def paste_callback(context_text):
+    content_text.event_generate('<<Copy>>')
+def paste_callback(content_text):
     """
     paster callback: implement paste function
-    context_text: Text object
+    content_text: Text object
     """
-    context_text.event_generate('<<Paste>>')
-def undo_callback(context_text):
+    content_text.event_generate('<<Paste>>')
+def undo_callback(content_text):
     """
     undo callback: implement undo function
-    context_text: Text object
+    content_text: Text object
     """
-    context_text.event_generate('<<Undo>>')
-def redo_callback(context_text):
+    content_text.event_generate('<<Undo>>')
+def redo_callback(content_text):
     """
     redo callback: implement redo fucntion
-    context_text: Text object
+    content_text: Text object
     """
-    context_text.event_generate('<<Redo>>')
+    content_text.event_generate('<<Redo>>')
 
-def select_all_callback(context_text, event=None):
+def select_all_callback(content_text, event=None):
     """
     select all callback: implement select all function
-    context_text: Text object
+    content_text: Text object
     """
     #just mark tag sel("selected") from beginning to the end of Text object 
-    context_text.tag_add('sel', '1.0', 'end')
-def find_text_callback(master, context_text, event=None):
+    content_text.tag_add('sel', '1.0', 'end')
+def find_text_callback(master, content_text, event=None):
     """
     find text callback: implement find text function
     master: the root frame(or toplevel window)
-    context_text: Text object
+    content_text: Text object
     """
     def close_search_window():
         """
         close_search_window: remove all tag and close the search window
         """
-        context_text.tag_remove('match', '1.0', END)
+        content_text.tag_remove('match', '1.0', END)
         search_toplevel.destroy()
         return "break"
     search_toplevel = Toplevel(master)
@@ -81,13 +82,13 @@ def find_text_callback(master, context_text, event=None):
         variable=ignore_case_value).grid(row=1, column=0, sticky='e')
     Button(search_toplevel, text="Find All", underline=0, 
         command=lambda: search_output(search_entry_widget.get(), ignore_case_value.get(),
-        context_text, search_toplevel, search_entry_widget)).grid(row=0, column=0,
+        content_text, search_toplevel, search_entry_widget)).grid(row=0, column=0,
                 sticky='e' + 'w', padx=2, pady=2)
     #we replace the close button of the find window by close_search_window function
     search_toplevel.protocol('WM_DELETE_WINDOW', close_search_window)
 
-def search_output(search_string, if_ignore_case, context_text, search_toplevel, search_box):
-    context_text.tag_remove('match', '1.0', END)
+def search_output(search_string, if_ignore_case, content_text, search_toplevel, search_box):
+    content_text.tag_remove('match', '1.0', END)
     """
     search_output: find search_string and add match tag to the matching text
     """
@@ -95,18 +96,59 @@ def search_output(search_string, if_ignore_case, context_text, search_toplevel, 
     if search_string:
         start_pos = '1.0'
         while True:
-            start_pos = context_text.search(search_string, start_pos, 
+            start_pos = content_text.search(search_string, start_pos, 
                 nocase=if_ignore_case, stopindex=END)
             if not start_pos:
                 break
             end_pos = '{}+{}c'.format(start_pos, len(search_string))
-            context_text.tag_add('match', start_pos, end_pos)
+            content_text.tag_add('match', start_pos, end_pos)
             matches_found += 1
             start_pos = end_pos
-            context_text.tag_config('match', foreground='red', background='yellow')
+            content_text.tag_config('match', foreground='red', background='yellow')
             search_box.focus_set()
             search_toplevel.title('{} matches found'.format(matches_found))
-    
+
+def open_file(master, content_text, file_name, event=None):
+    input_file_name = askopenfilename(defaultextension='.txt',
+        filetypes=[('All Files', '*.*'), ('Text Documents', '*.txt')])
+    if input_file_name:
+        file_name = input_file_name
+        master.title('{} - {}'.format(os.path.basename(file_name), 
+            'Simple Text Editor'))
+        content_text.delete(1.0, END)
+        with open(file_name) as _file:
+            content_text.insert(1.0, _file.read())
+    return "break"
+
+def write_to_file(content_text, file_name):
+    try:
+        content = content_text.get(1.0, END)
+        with open(file_name, 'w') as tmp_file:
+            tmp_file.write(content)
+    except IOError:
+        pass
+
+def save(master, content_text, file_name, event=None):
+    if not file_name:
+        save_as(content_text, file_name)
+    else:
+        write_to_file(content_text, file_name)
+    return "break"
+
+def save_as(master, content_text, file_name, event=None):
+    input_file_name = asksaveasfilename(defaultextension=".txt",
+        filetypes=[('All Files', "*.*"), ("Text Documents", ".txt")])
+    if input_file_name:
+        file_name = input_file_name
+        write_to_file(content_text, file_name)
+        master.title('{} - {}'.format(os.path.basename(file_name), 'Simple Text Editor'))
+    return "break"
+
+def new_file(master, content_text, file_name, event=None):
+    master.title('Untitled')
+    content_text.delete(1.0, END)
+    file_name = None
+
 class Editor(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
@@ -132,56 +174,75 @@ class Editor(Frame):
         self.line_number_bar = Text(self.master, width=4, padx=3, takefocus=0, border=0,
             state='disabled', wrap='none')
         self.line_number_bar.pack(expand='no', fill='y', side='left')
-        self.context_text = Text(self.master, wrap='word', undo=1)
-        self.context_text.pack(expand='yes', fill='both')
-        self.scroll_bar = Scrollbar(self.context_text)
-        self.context_text.config(yscrollcommand=self.scroll_bar.set)
-        self.scroll_bar.config(command=self.context_text.yview)
+        self.content_text = Text(self.master, wrap='word', undo=1)
+        self.content_text.pack(expand='yes', fill='both')
+        self.scroll_bar = Scrollbar(self.content_text)
+        self.content_text.config(yscrollcommand=self.scroll_bar.set)
+        self.scroll_bar.config(command=self.content_text.yview)
         self.scroll_bar.pack(side='right', fill='y')
-
+        self.file_name = None
         #File menu
-        self.file_menu.add_command(label='New', accelerator='Ctrl+N', compound='left', command=quit)
-        self.file_menu.add_command(label='Open', accelerator='Ctrl+O', compound='left', command=quit)
-        self.file_menu.add_command(label='Save', accelerator='Ctrl+S', compound='left', command=quit)
-        self.file_menu.add_command(label='Save as', accelerator='Shift+Ctrl+N', 
-            compound='left', command=quit)
+        self.file_menu.add_command(label='New', accelerator='Ctrl+N', 
+            compound='left', command=lambda: new_file(self.master, self.content_text, self.file_name))
+        self.file_menu.add_command(label='Open', accelerator='Ctrl+O', compound='left', 
+            command=lambda: open_file(self.master, self.content_text, self.file_name))
+        self.file_menu.add_command(label='Save', accelerator='Ctrl+S', compound='left', 
+            command=lambda: save(self.master, self.content_text, self.file_name))
+        self.file_menu.add_command(label='Save as', accelerator='Shift+Ctrl+S', 
+            compound='left', command= lambda: save_as(self.master, self.content_text, self.file_name))
         self.file_menu.add_separator()
         self.file_menu.add_command(label='Exit', accelerator='Alt+F4', compound='left', command=quit)
         #edit menu
         self.edit_menu.add_command(label='Undo', accelerator='Ctrl+Z', compound='left', 
-            command=lambda : undo_callback(self.context_text))
+            command=lambda : undo_callback(self.content_text))
         self.edit_menu.add_command(label='Redo', accelerator='Ctrl+Y', compound='left', 
-            command=lambda : redo_callback(self.context_text))
+            command=lambda : redo_callback(self.content_text))
         self.edit_menu.add_command(label='Cut', accelerator='Ctrl+X', compound='left', 
-            command=lambda : cut_callback(self.context_text))
+            command=lambda : cut_callback(self.content_text))
         self.edit_menu.add_command(label='Copy', accelerator='Ctrl+C', compound='left', 
-            command=lambda :copy_callback(self.context_text))
+            command=lambda :copy_callback(self.content_text))
         self.edit_menu.add_command(label='Paste', accelerator='Ctrl+V', compound='left', 
-            command=lambda : paste_callback(self.context_text))
+            command=lambda : paste_callback(self.content_text))
         self.edit_menu.add_command(label='Select All', accelerator='Ctrl+A', 
-            compound='left', command=lambda: select_all_callback(self.context_text))
+            compound='left', command=lambda: select_all_callback(self.content_text))
         self.edit_menu.add_command(label='Find', accelerator='Ctrl+F',
-            compound='left', command=lambda: find_text_callback(self.master, self.context_text))
+            compound='left', command=lambda: find_text_callback(self.master, self.content_text))
         
-        #We bind shortcut with the function correspondence
-        self.context_text.bind('<Control-x>', cut_callback(self.context_text))
-        self.context_text.bind('<Control-X>', cut_callback(self.context_text))
-        self.context_text.bind('<Control-c>', copy_callback(self.context_text))
-        self.context_text.bind('<Control-C>', copy_callback(self.context_text))
-        self.context_text.bind('<Control-v>', paste_callback(self.context_text))
-        self.context_text.bind('<Control-V>', paste_callback(self.context_text))
-        self.context_text.bind('<Control-z>', undo_callback(self.context_text))
-        self.context_text.bind('<Control-Z>', undo_callback(self.context_text))
-        self.context_text.bind('<Control-Y>', redo_callback(self.context_text))
-        self.context_text.bind('<Control-Y>', redo_callback(self.context_text))
-        self.context_text.bind('<Control-A>', 
-            lambda:select_all_callback(self.context_text))
-        self.context_text.bind('<Control-a>', 
-            lambda:select_all_callback(self.context_text))
-        self.context_text.bind('<Control-F>',
-            lambda x: find_text_callback(self.master, self.context_text))
-        self.context_text.bind('<Control-f>',
-            lambda x: find_text_callback(self.master, self.context_text))
+        #We bind the shortcut with the function correspondence
+        self.content_text.bind('<Control-x>', lambda: cut_callback(self.content_text))
+        self.content_text.bind('<Control-X>', lambda: cut_callback(self.content_text))
+        self.content_text.bind('<Control-c>', lambda: copy_callback(self.content_text))
+        self.content_text.bind('<Control-C>', lambda: copy_callback(self.content_text))
+        self.content_text.bind('<Control-v>', lambda: paste_callback(self.content_text))
+        self.content_text.bind('<Control-V>', lambda: paste_callback(self.content_text))
+        self.content_text.bind('<Control-z>', lambda: undo_callback(self.content_text))
+        self.content_text.bind('<Control-Z>', lambda: undo_callback(self.content_text))
+        self.content_text.bind('<Control-Y>', lambda: redo_callback(self.content_text))
+        self.content_text.bind('<Control-Y>', lambda: redo_callback(self.content_text))
+        self.content_text.bind('<Control-A>', 
+            lambda:select_all_callback(self.content_text))
+        self.content_text.bind('<Control-a>', 
+            lambda:select_all_callback(self.content_text))
+        self.content_text.bind('<Control-F>',
+            lambda x: find_text_callback(self.master, self.content_text))
+        self.content_text.bind('<Control-f>',
+            lambda x: find_text_callback(self.master, self.content_text))
+        self.content_text.bind('<Control-O>', 
+            lambda: open_file(self.master, self.content_text, self.file_name))
+        self.content_text.bind('<Control-o>', 
+            lambda: open_file(self.master, self.content_text, self.file_name))
+        self.content_text.bind('<Control-S>',
+            lambda: save(self.master, self.content_text, self.file_name))
+        self.content_text.bind('<Control-s>',
+            lambda: save(self.master, self.content_text, self.file_name))
+        self.content_text.bind('<Shift-Control-S>',
+            lambda: save_as(self.master, self.content_text, self.file_name))
+        self.content_text.bind('<Shift-Control-s>',
+            lambda: save_as(self.master, self.content_text, self.file_name))
+        self.content_text.bind('<Control-n>',
+            lambda: new_file(self.master, self.content_text, self.file_name))
+        self.content_text.bind('<Control-N', 
+            lambda: new_file(self.master, self.content_text, self.file_name))
 
 if __name__ == '__main__':
     root = Tk()
